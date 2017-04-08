379d5a607256b6136a4cc81bf27413e9143acada
Alan Magelssen <alanmagelssen@gmail.com>
2017-04-07 22:18:24 -0700

    save date to db and correct formatting

--- app/components/new-article.js
+++ app/components/new-article.js
@@ -8,7 +8,6 @@ export default Ember.Component.extend({
       var params = {
         title: this.get('title') ? this.get('title') : "",
         author: this.get('author') ? this.get('author') : "",
-        date: this.get('date') ? this.get('date') : "",
         description: this.get('description') ? this.get('description') : "",
         text: this.get('text') ? this.get('text') : "",
         photo: this.get('photo') ? this.get('photo') : "",
